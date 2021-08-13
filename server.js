d006252d677dedae8b5f2e13c656ae5735bd9e7c
Võ Ngọc Bảo <vnbao.khtn@gmail.com>
2021-08-13 20:12:31 +0700

    Resolve all of problems

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
-var app = express();
-
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const dotenv = require('dotenv');
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+dotenv.config();
+const app = express();
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 });
 
+app.use(express.json());
+app.use(cors());
+app.disable('x-powered-by');
+// app.use(express.static('public'));
+app.use('/public', express.static(process.cwd() + '/public'));
+// app.use(upload.single('files'));
+app.use(express.urlencoded({ extended: false }));
 
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const uploadFile = req.file;
+
+  res.send({
+    name: uploadFile.originalname
+    , type: uploadFile.mimetype
+    , size: uploadFile.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
